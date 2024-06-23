59c15c61507599d0c4f5f6235d11f0614cf03bf9
JacksonLDJ <104440080+JacksonLDJ@users.noreply.github.com>
2024-06-23 23:08:13 +0100

    Update CTA.jsx

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -1,12 +1,11 @@
 // CTA.jsx
 import React from 'react';
 import { Link } from 'react-scroll';
-import CV from '../../assets/CV.pdf';
 
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={} download className="btn">Download CV</a>
+      <btn download className="btn">Download CV</btn>
       <a href="mailto:me@liamjackson.co.uk" className='btn'>Email Me</a>
       <Link to="Portfolio" smooth={true} duration={500} className='btn'>Portfolio</Link>
     </div>
