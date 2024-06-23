49c679c23ab4554836c9b6e510688938083b9478
JacksonLDJ <104440080+JacksonLDJ@users.noreply.github.com>
2024-06-23 23:05:41 +0100

    Update CTA.jsx

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -6,7 +6,7 @@ import CV from '../../assets/CV.pdf';
 const CTA = () => {
   return (
     <div className='cta'>
-      <a href={CV} download className="btn">Download CV</a>
+      <a href={} download className="btn">Download CV</a>
       <a href="mailto:me@liamjackson.co.uk" className='btn'>Email Me</a>
       <Link to="Portfolio" smooth={true} duration={500} className='btn'>Portfolio</Link>
     </div>
