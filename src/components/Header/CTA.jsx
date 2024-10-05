28cc8aefd31444d47306ecfcaaaf34fdbbee8a87
JacksonLDJ <104440080+JacksonLDJ@users.noreply.github.com>
2024-10-05 22:37:42 +0100

    Update CTA.jsx

--- src/components/Header/CTA.jsx
+++ src/components/Header/CTA.jsx
@@ -7,7 +7,6 @@ const CTA = () => {
 
   return (
     <div className='cta'>
-      <a href="/Website-CV.pdf" download className="btn">Download CV</a>
       <a href="mailto:me@liamjackson.co.uk" className='btn'>Email Me</a>
       <button onClick={() => navigate("/blog") } className="btn">Blog Posts</button>
     </div>
