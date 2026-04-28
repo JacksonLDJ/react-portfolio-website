5d8effeebe8133f6c2f7829089b8dcc51c073e70
JacksonLDJ <104440080+JacksonLDJ@users.noreply.github.com>
2026-04-28 08:46:01 +0100

    Update markdownPreview.js

--- src/utils/markdownPreview.js
+++ src/utils/markdownPreview.js
@@ -2,7 +2,7 @@ export function markdownPreview(markdown, maxLength = 80) {
   if (!markdown) return "";
 
   const plainText = markdown
-    .replace(/!\[([^\]]*)\]\([^)]+\)/g, "$1")
+    .replace(/!\[([^\]]*)\]\([^)]+\)/g, "")
     .replace(/\[([^\]]+)\]\([^)]+\)/g, "$1")
     .replace(/`{1,3}[^`]*`{1,3}/g, "")
     .replace(/^#{1,6}\s+/gm, "")
